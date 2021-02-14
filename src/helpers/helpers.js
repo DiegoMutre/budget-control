1ec4099017b312f2ce2907ff2add3529af99d1d8
DiegoMutre <tysonmutre10@gmail.com>
2021-02-14 18:38:18 -0500

    feat: Create helper function that return an alert class.

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,3 +1,19 @@
 export function isValidAmount(number = 0) {
     return number > 0 || isNaN(number);
 }
+
+export function getAlertColor(budget, remaining) {
+    let alertClassName = "alert-success";
+
+    // Calculate 25%
+    if (remaining / 4 <= budget) {
+        return (alertClassName = "alert-danger");
+    }
+
+    // Calculate 50%
+    if (remaining / 2 <= budget) {
+        return (alertClassName = "alert-warning");
+    }
+
+    return alertClassName;
+}
