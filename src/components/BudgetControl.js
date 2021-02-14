90063b43ee7a56c6f0454f3c91b3b7ff8a3027e6
DiegoMutre <tysonmutre10@gmail.com>
2021-02-14 18:52:25 -0500

    feat: Use 'getAlertColor' helper in BudgetControl component.

--- src/components/BudgetControl.js
+++ src/components/BudgetControl.js
@@ -1,10 +1,17 @@
 import PropTypes from "prop-types";
+import { useRef } from "react";
+import { getAlertColor } from "../helpers/helpers";
 
 const BudgetControl = ({ amounts: { budget, remaining } }) => {
+    const alertClassName = useRef("alert-success");
+    alertClassName.current = getAlertColor(budget, remaining);
+
     return (
         <>
             <div className="alert alert-primary">Budget: ${budget}</div>
-            <div className="alert alert-success">Remaining: ${remaining}</div>
+            <div className={`alert ${alertClassName.current}`}>
+                Remaining: ${remaining}
+            </div>
         </>
     );
 };
