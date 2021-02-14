2e6935b7eee4dd932442d3f0d893044cf13e7872
DiegoMutre <tysonmutre10@gmail.com>
2021-02-14 16:17:38 -0500

    feat: Create ControlContainer component.

---
+++ src/components/ControlContainer.js
@@ -0,0 +1,12 @@
+const ControlContainer = () => {
+    return (
+        <div className="row">
+            <div className="one-half column">{/* ExpenseForm here */}</div>
+            <div className="one-half column">
+                {/* ExpenseList here and BudgetControl here */}
+            </div>
+        </div>
+    );
+};
+
+export default ControlContainer;
