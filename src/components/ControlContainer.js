23ac0739d7b467f6adc79ca4af1d3fdfd790fb42
DiegoMutre <tysonmutre10@gmail.com>
2021-02-14 16:39:10 -0500

    feat: Create 'expense' state for ControlContainer component.

--- src/components/ControlContainer.js
+++ src/components/ControlContainer.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 const ControlContainer = () => {
+    const [expenses, setExpenses] = useState([]);
+
     return (
         <div className="row">
             <div className="one-half column">
-                <ExpenseForm />
+                <ExpenseForm setExpenses={setExpenses} />
             </div>
             <div className="one-half column">
                 {/* ExpenseList here and BudgetControl here */}
