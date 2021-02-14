9afc7f84a406af911fe5a88063d6a2981ec2fa70
DiegoMutre <tysonmutre10@gmail.com>
2021-02-14 14:30:33 -0500

    feat: Use Form component in App component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Form from "./components/Form";
+
 function App() {
     return (
         <div className="container">
             <header>
                 <h1>Weekly Expense</h1>
                 <div className="contenido-principal contenido">
-                    {/* Form here */}
+                    <Form />
                 </div>
             </header>
         </div>
