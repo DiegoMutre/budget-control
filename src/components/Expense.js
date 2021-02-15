01429b7309a8fc632e166fe21afe9381bac6fa31
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 11:21:33 -0500

    feat: Add event that delete an expense.

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,10 +1,17 @@
 import PropTypes from "prop-types";
 
-const Expense = ({ expense: { name, amount } }) => {
+const Expense = ({ expense: { id, name, amount }, deleteExpense }) => {
+    const handleDelete = () => {
+        deleteExpense(id);
+    };
+
     return (
         <li className="gastos">
             <p>
                 {name} <span className="gasto">${amount}</span>
+                <button className="alert-danger" onClick={handleDelete}>
+                    Delete
+                </button>
             </p>
         </li>
     );
@@ -12,6 +19,7 @@ const Expense = ({ expense: { name, amount } }) => {
 
 Expense.propTypes = {
     expense: PropTypes.object.isRequired,
+    deleteExpense: PropTypes.func.isRequired,
 };
 
 export default Expense;
